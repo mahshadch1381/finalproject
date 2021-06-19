1e3148551e746eabcacb7b499d3db810d85fd845
mahshad <mahshadch1381@gmail.com>
2021-06-19 16:36:19 +0430

    posting details added

--- Client/postingClient.java
+++ Client/postingClient.java
@@ -1,4 +1,33 @@
 package Client;
 
+import model.Post;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class postingClient {
+    public static int port=113;
+    public Post p;
+    public String a= "1";
+    public postingClient(Post post){
+        p=post;
+    }
+    public String posting_connection() throws IOException, ClassNotFoundException {
+        Socket socket=new Socket("127.0.0.1",port);
+        ObjectInputStream objectInputStream=new ObjectInputStream(socket.getInputStream());
+        ObjectOutputStream objectOutputStream=new ObjectOutputStream(socket.getOutputStream());
+        String message=p.publisher+"#"+p.title+"#"+p.description+"#"+p.date+"#"+p.picture;
+        objectOutputStream.writeObject(message);
+        objectOutputStream.flush();
+        Object answer=objectInputStream.readObject();
+        a="0";
+        objectOutputStream.writeObject(a);
+        objectOutputStream.flush();
+        objectInputStream.close();
+        objectOutputStream.close();
+        return (String) answer;
+    }
+
 }
