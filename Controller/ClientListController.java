24c295fb43314259367c56ba50be300d485c0996
mahshad <mahshadch1381@gmail.com>
2021-06-20 11:41:53 +0430

    3 new files added

---
+++ Controller/ClientListController.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class ClientListController {
+
+    public ListView list;
+    @FXML
+    public void initialize(){
+
+    }
+}
