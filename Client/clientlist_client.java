d79ce3715ad0362eb4726bd780f148b23efac220
mahshad <mahshadch1381@gmail.com>
2021-06-20 12:13:02 +0430

    client and server class of clientslist complited

--- Client/clientlist_client.java
+++ Client/clientlist_client.java
@@ -1,4 +1,27 @@
 package Client;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.List;
+
 public class clientlist_client {
+    public static int port=114;
+    String a="1";
+    public List<String> findingClientList() throws IOException, ClassNotFoundException {
+        Socket socket=new Socket("127.0.0.1",port);
+        ObjectInputStream objectInputStream=new ObjectInputStream(socket.getInputStream());
+        ObjectOutputStream objectOutputStream=new ObjectOutputStream(socket.getOutputStream());
+        String message="find the list";
+        objectOutputStream.writeObject(message);
+        objectOutputStream.flush();
+        Object answer=objectInputStream.readObject();
+        a="0";
+        objectOutputStream.writeObject(a);
+        objectOutputStream.flush();
+        objectInputStream.close();
+        objectOutputStream.close();
+        return (List<String>) answer;
+    }
 }
